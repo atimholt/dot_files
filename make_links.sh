1365e7613734d8531f3406a0cb7200bef832f9b1
Tim Holt <atimholt@gmail.com>
2014-02-16 20:19:00 -0800

    Created install script and blank dot files.

---
+++ make_links.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ┌─────────────────────────────────────────────────────────────────────────┐
+# │make_links.sh                                                            │
+# │                                                                         │
+# │For the symlinking of the dot_files in this repo into the home folder.   │
+# │For use upon a fresh OS install or upon the addition of another dot file.│
+# └─────────────────────────────────────────────────────────────────────────┘
+
+#│-v-1 │ Variables
+#└─────┴───────────
+
+dot_files_dir=~/dot_files
+old_files_dir=~/dotfiles_old
+dot_files="bashrc vimrc hgrc"
+
+#│-v-1 │ Main Body of Script
+#└─────┴─────────────────────
+
+mkdir -p $old_files_dir/
+
+cd $dot_files_dir/
+
+for dot_file in $dot_files; do
+    mv ~/.$dot_file $old_files_dir/
+    ln -s $dot_files_dir/$dot_file ~/.$file
+done
+
+# vim: set ft=sh fmr=-v-,-^- fdm=marker cms=#%s et ts=4 sw=4 sts=4 :
