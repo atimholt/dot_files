d1ccd0dbfb857e67bcd10691585546eb39a5d73b
Tim Holt <atimholt@gmail.com>
2014-02-19 18:50:22 -0800

    Made make_links.sh a little more versatile, in prep for non-conflict with Powershell equivalent.

--- make_links.sh
+++ make_links.sh
@@ -9,15 +9,24 @@
 
 #│-v-1 │ To-Do's
 #└─────┴─────────
-# TODO rework file names and write another (powershell) script for multi-system
-#      compatability and automatic generation of variable "dot_files"
+# TODO Write a Powershell script for the same purpose, in Windows.
+# TODO command line option for non-preservation of destination dot files.
 
 #│-v-1 │ Variables
 #└─────┴───────────
 
-dot_files_dir=~/dot_files
+# For getting this script’s directory, with caveats.
+# From: http://stackoverflow.com/a/246128/837406
+dot_files_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 old_files_dir=~/dot_files_old
-dot_files="bashrc vimrc hgrc gitconfig"
+
+declare -A dot_files
+
+dot_files["bashrc"]=".bashrc"
+dot_files["vimrc"]=".vimrc"
+dot_files["hgrc"]=".hgrc"
+dot_files["gitconfig"]=".gitconfig"
 
 #│-v-1 │ Main Body of Script
 #└─────┴─────────────────────
@@ -26,9 +35,10 @@ mkdir -p $old_files_dir/
 
 cd $dot_files_dir/
 
-for dot_file in $dot_files; do
-    mv ~/.$dot_file $old_files_dir/
-    ln -s $dot_files_dir/$dot_file ~/.$dot_file
+for dot_file in ${!dot_files[@]}; do
+    ln -b -s $dot_files_dir/$dot_file ~/${dot_files["$dot_file"]}
 done
+
+exit 0
 
 # vim: set ft=sh fmr=-v-,-^- fdm=marker cms=#%s et ts=4 sw=4 sts=4 :
