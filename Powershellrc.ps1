d5aefbf07ec8e5724f137b5ca9242905180d61a4
Tim Holt <atimholt@gmail.com>
2019-07-22 13:41:04 -0700

    Add Chocolatey stuff
    One of the Python things I've installed lately (I'm getting into Jupyter Lab)
    automatically installed Chocolatey. 'sfine with me.

--- Powershellrc.ps1
+++ Powershellrc.ps1
@@ -279,3 +279,9 @@ function prompt
 
 # vim: set fmr=▼,▲ fdm=marker cms=#%s et ts=2 sw=0 sts=0 :
 
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
